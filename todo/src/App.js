6388d1d5db1858d87e62075415f77f214607fb67
Banu Seyhan <banuaskahraman@gmail.com>
2022-12-06 10:51:01 +0300

    ok

--- todo/src/App.js
+++ todo/src/App.js
@@ -234,12 +234,8 @@ function App() {
 
       <footer className="info">
         <p>Click to edit a todo</p>
-        <p>
-          Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
-        </p>
-        <p>
-          Part of <a href="http://todomvc.com">TodoMVC</a>
-        </p>
+        
+        
       </footer>
     </>
   );
